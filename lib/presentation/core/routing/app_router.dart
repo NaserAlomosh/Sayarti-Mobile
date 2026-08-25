99dac9564ce52d7e9d11658824074f3722b93ccb
Naser Alomosh <132396593+NaserAlomosh@users.noreply.github.com>
2026-08-25 18:13:09 +0300

    Implement Figma splash view

--- lib/presentation/core/routing/app_router.dart
+++ lib/presentation/core/routing/app_router.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:sayarti_mobile/presentation/core/routing/routes.dart';
+import 'package:sayarti_mobile/presentation/features/splash/presentation/view/splash_view.dart';
 
 class AppRouter {
   static Route<dynamic>? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-    
+      case Routes.splashView:
+        return CupertinoPageRoute<void>(
+          settings: settings,
+          builder: (_) => const SplashView(),
+        );
     }
     return null;
   }
